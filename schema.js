28fe0efa3901158658df376e9b0389ef79d1d858
vprots <v.prots@yaware.com>
2019-06-04 14:24:23 +0300

    created rootQuery && fetching data

--- schema.js
+++ schema.js
@@ -1,12 +1,71 @@
 const { 
-    GraphQlObjectType,
-    GraphQlString,
-    GraphQlInt,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
     GraphQLSchema,
-    GraphQlList,
-    GraphNonNull
+    GraphQLList,
+    GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields: () => ({
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        email: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
+    })
+});
 
-})
+const customers = [
+    {
+        id: '1',
+        name: 'Test test',
+        email: 'test@gmail.com',
+        age: 32,
+    },
+    {
+        id: '2',
+        name: 'Test test 2',
+        email: 'test2@gmail.com',
+        age: 23,
+    },
+]
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                for (let i = 0; i < customers.length; i++) {
+                    if (customers[i].id == args.id) {
+                        return customers[i];
+                    }
+                }
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return customers;
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
